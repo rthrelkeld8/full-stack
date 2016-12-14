eb4ac206cbb675f6ce2a6c5208c6e03b72d0b33a
Richard Threlkeld <Richard Threlkeld>
2016-12-13 19:35:52 -0600

    database updates

---
+++ GigHub/GigHub/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GigHub.Models
+{
+    public class Genre
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+    }
+}
