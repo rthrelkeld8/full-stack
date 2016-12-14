2b2403f3c7de95c8644cb76c8fc488c7e9926d3a
Richard Threlkeld <Richard Threlkeld>
2016-12-13 22:33:01 -0600

    form cleanup

--- GigHub/GigHub/Models/Gig.cs
+++ GigHub/GigHub/Models/Gig.cs
@@ -6,8 +6,10 @@ namespace GigHub.Models
     public class Gig
     {
         public int Id { get; set; }
-        [Required]
         public ApplicationUser Artist { get; set; }
+
+        [Required]
+        public string ArtistId { get; set; }
 
         public DateTime DateTime { get; set; }
 
@@ -15,8 +17,10 @@ namespace GigHub.Models
         [StringLength(255)]
         public string Venue { get; set; }
 
-        [Required]
         public Genre Genre { get; set; }
+
+        [Required]
+        public byte GenreId { get; set; }
     }
 
 }
